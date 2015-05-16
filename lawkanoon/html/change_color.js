4ebcc71ed4ee35940ec5d1d840d14151fa404096
sinlord <candid.abhinav@gmail.com>
2015-05-16 19:00:19 +0200

    added JS validations

---
+++ lawkanoon/html/change_color.js
@@ -0,0 +1,13 @@
+/**
+ * Created by abhinavverma on 5/16/15.
+ */
+
+function changeColor(){
+    document.getElementById("jstest").style.color="red";
+    return true;
+}
+function changeAgain(){
+    document.getElementById("jstest").style.color="black";
+    return true;
+}
+
